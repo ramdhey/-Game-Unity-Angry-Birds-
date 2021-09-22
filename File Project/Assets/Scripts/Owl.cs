27750f88dea30cf4feacf8af042bef5ca9994f52
Rama Dhea Yudhistira <90076976+ramdhey@users.noreply.github.com>
2021-09-22 14:31:57 +0700

    Angry Birds Tugas Chapter5[413-RamaDheaYudhistria]

---
+++ File Project/Assets/Scripts/Owl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    public float fieldofImpact;
+
+    public float force;
+
+    public LayerMask LayerToHit;
+
+    public GameObject ExplosionEffect;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            explode();
+        }
+    }
+    private void explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact, LayerToHit);
+        foreach (Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+        }
+     GameObject ExplosionEffectIns = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+        Destroy(ExplosionEffectIns, 5);
+        
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldofImpact);
+    }
+}
